1e73eed91cd8f1b25650a746ff93f838c65a3ce9
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-04-14 15:57:13 +0200

    Update to_base64.py

--- to_base64.py
+++ to_base64.py
@@ -1,15 +1,50 @@
 import base64
+import subprocess
 
-def convert_font_to_base64(font_path):
-    with open(font_path, "rb") as font_file:
+def convert_font_to_base64(font_base_name):
+    with open(f"src/{font_base_name}-without-glyf.ttf", "rb") as font_file:
         font_data = font_file.read()
         base64_encoded_data = base64.b64encode(font_data)
         base64_string = base64_encoded_data.decode('utf-8')
-    return base64_string
+    
+    with open(f"src/{font_base_name}-without-glyf.ttf.base64.txt", 'w') as f:
+        f.write(base64_string)
+
+def remove_glyf_table(font_base_name):
+    command = f'ttx src/{font_base_name}.ttf'
+    print(command)
+    print(subprocess.run(command, shell=True))
+
+    command = f"sed -i '/<glyf>/,/<\/glyf>/d' src/{font_base_name}.ttx"
+    print(command)
+    print(subprocess.run(command, shell=True))
+
+    command = f"ttx -o src/{font_base_name}-without-glyf.ttf src/{font_base_name}.ttx"
+    print(command)
+    print(subprocess.run(command, shell=True))
+
+def cleanup(font_base_name):
+    command = f'rm src/{font_base_name}.ttx src/{font_base_name}-without-glyf.ttf'
+    print(command)
+    print(subprocess.run(command, shell=True))
 
 
-font_path = "src/NotoSansMyanmar-Regular-without-glyf.ttf"
 
-base64_font = convert_font_to_base64(font_path)
-with open(f"{font_path}.base64.txt", 'w') as f:
-    f.write(base64_font)
+font_base_names = [
+    'NotoSansDevanagari-Regular',
+    'NotoSansGujarati-Regular',
+    'NotoSansGurmukhi-Regular',
+    'NotoSansKannada-Regular',
+    'NotoSansKhmer-Regular',
+    'NotoSansMyanmar-Regular',
+    'NotoSansOriya-Regular',
+    'NotoSansBengali-Regular',
+    'NotoSansMalayalam-Regular',
+    'NotoSansTelugu-Regular',
+    'NotoSansTamil-Regular'
+]
+
+for font_base_name in font_base_names:
+    remove_glyf_table(font_base_name)
+    convert_font_to_base64(font_base_name)
+    cleanup(font_base_name)
